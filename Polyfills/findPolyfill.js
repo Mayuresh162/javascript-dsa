cdd72e74e2c05682ba36602475446469add30d5a
Mayuresh Bhagat <mayureshbhagat0@gmail.com>
2023-06-02 11:34:16 +0530

    Add files via upload

---
+++ Polyfills/findPolyfill.js
@@ -0,0 +1,14 @@
+if (!Array.prototype.customFind) {
+    Array.prototype.customFind = function(cb) {
+        for (let i=0; i<this.length; i++) {
+            if(cb(this[i], i, this)) {
+                return this[i]
+            }
+        }
+
+        return undefined;
+    }
+}
+const numbers = [2, 4, 5, 6, 7, 9];
+const findFirstOddCustom = numbers.customFind(num => num % 2);
+console.log(findFirstOddCustom); // 5
