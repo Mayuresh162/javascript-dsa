6dfb1e15ab1b63f51f3898d88c294619c981da4d
Mayuresh Bhagat <mayureshbhagat0@gmail.com>
2023-06-02 11:36:36 +0530

    Update findPolyfill.js

--- Polyfills/findPolyfill.js
+++ Polyfills/findPolyfill.js
@@ -2,7 +2,7 @@ if (!Array.prototype.customFind) {
     Array.prototype.customFind = function(cb) {
         for (let i=0; i<this.length; i++) {
             if(cb(this[i], i, this)) {
-                return this[i]
+                return this[i];
             }
         }
 
