67ae6328ffd81b273e91c0876ce990720689f826
Mayuresh Bhagat <mayureshbhagat0@gmail.com>
2025-10-14 17:27:29 +0530

    Update stockBuySell.js

--- A2Z_DSA/Arrays/Medium/stockBuySell.js
+++ A2Z_DSA/Arrays/Medium/stockBuySell.js
@@ -18,7 +18,21 @@ function stockBuySell(arr) {
         max = Math.max(max, elem - min);
     }
     console.log(max);
+
+    // another optimnal approach
+    let max = 0;
+    let min = prices[0];
+    for (let i=0; i<prices.length; i++) {
+        if (prices[i] - min > max) {
+            max = prices[i] - min;
+        }
+        if (prices[i] < min) {
+            min = prices[i];
+        }
+    }
+    console.log(max);
 }
 
 stockBuySell([7,1,5,3,6,4]);
+
 stockBuySell([7,6,4,3,1]);
