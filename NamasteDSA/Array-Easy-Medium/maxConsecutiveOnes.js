da1df8a73baaa967fdf03d94eeab17f263f3b1da
Mayuresh Bhagat <mayureshbhagat0@gmail.com>
2025-11-05 16:05:15 +0530

    Update maxConsecutiveOnes.js

--- NamasteDSA/Array-Easy-Medium/maxConsecutiveOnes.js
+++ NamasteDSA/Array-Easy-Medium/maxConsecutiveOnes.js
@@ -1,9 +1,36 @@
 var findMaxConsecutiveOnes = function (nums) {
-  let count = 0;
-  let max = 0;
-  for (let elem of nums) {
-    count = elem === 1 ? ++count : 0;
-    max = Math.max(max, count);
+  // one way
+  // let count = 0;
+  // let max = 0;
+  // for (let elem of nums) {
+  //   count = elem === 1 ? ++count : 0;
+  //   max = Math.max(max, count);
+  // }
+  // return max;
+
+  // other way
+  // let curr = 0;
+  // let res = 0;
+  // for (let i of nums) {
+  //   if (i === 0) {
+  //     curr = 0;
+  //   } else {
+  //     curr++;
+  //     res = Math.max(curr, res);
+  //   }
+  // }
+  // return res;
+
+  // other way
+  let currCount = 0;
+  let maxCount = 0;
+  for (let i = 0; i < nums.length; i++){
+      if (nums[i] === 1){
+          currCount++;
+      } else {
+          maxCount = Math.max(currCount, maxCount);
+          currCount = 0;
+      }
   }
-  return max;
+  return Math.max(currCount, maxCount);
 };
