d952a0c8484fea2b1de5499377b5326fa6dacd83
Mayuresh Bhagat <mayureshbhagat0@gmail.com>
2025-11-05 16:01:06 +0530

    Update maxConsecutiveOnes.js

--- NamasteDSA/Array-Easy-Medium/maxConsecutiveOnes.js
+++ NamasteDSA/Array-Easy-Medium/maxConsecutiveOnes.js
@@ -1,6 +1,6 @@
 var findMaxConsecutiveOnes = function (nums) {
   let count = 0;
-  let max = -Infinity;
+  let max = 0;
   for (let elem of nums) {
     count = elem === 1 ? ++count : 0;
     max = Math.max(max, count);
