dd594c13e4bb3af522fc907187839e8ef1ed3cab
Mayuresh Bhagat <mayureshbhagat0@gmail.com>
2026-03-10 18:55:20 +0530

    Update DP_fibonacci_memoized.js

--- exercise/DP_fibonacci_memoized.js
+++ exercise/DP_fibonacci_memoized.js
@@ -5,11 +5,18 @@
 //     memo[n] = res;
 //     return res;
 // }
-
+let store = {};
 function fib(n, memo = [undefined, 1, 1]) {
-    if (memo[n] !== undefined) return memo[n];
-    // if (n<=2) return 1;
-    var res = fib(n-1, memo) + fib(n-2, memo);
-    memo[n] = res;
-    return res;
+    // if (memo[n] !== undefined) return memo[n];
+    // // if (n<=2) return 1;
+    // var res = fib(n-1, memo) + fib(n-2, memo);
+    // memo[n] = res;
+    // return res;
+
+    // other way
+    if (n<=2) return 1;
+    if (!store[n]) {
+        store[n] = fib(n-1) + fib(n-2);
+    }
+    return store[n];
 }
