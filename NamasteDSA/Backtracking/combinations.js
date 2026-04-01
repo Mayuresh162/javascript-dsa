0f2a7f8a893e37195d0a522775d60657519ac0d5
Mayuresh Bhagat <mayureshbhagat0@gmail.com>
2026-04-01 21:28:08 +0530

    Create combinations.js

---
+++ NamasteDSA/Backtracking/combinations.js
@@ -0,0 +1,17 @@
+var combine = function(n, k) {
+    const result = [];
+
+    function backtrack(start, path) {
+        if (path.length === k) {
+            result.push([...path]);
+            return;
+        }
+        for (let i=start; i<=n; i++) {
+            path.push(i);
+            backtrack(i+1, path);
+            path.pop();
+        }
+    }
+    backtrack(1, []);
+    return result;
+};
