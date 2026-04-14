719a821ee3c44ab22e62e664f6dc31f71959f157
Mayuresh Bhagat <mayureshbhagat0@gmail.com>
2026-04-14 20:05:54 +0530

    Refactor flatten function to use spread operator

--- exercise/flatten.js
+++ exercise/flatten.js
@@ -3,7 +3,8 @@ function flatten(oldArr){
     var newArr = []
     for(var i = 0; i < oldArr.length; i++){
           if(Array.isArray(oldArr[i])){
-                newArr = newArr.concat(flatten(oldArr[i]))
+                // newArr = newArr.concat(flatten(oldArr[i]))
+                  newArr = [...newArr, ...flatten(oldArr[i])];
           } else {
                 newArr.push(oldArr[i])
           }
