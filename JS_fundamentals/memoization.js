d9d22f244b8e73a38cee7902f902cf87826fe21b
Mayuresh Bhagat <mayureshbhagat0@gmail.com>
2026-05-04 10:21:31 +0530

    Add memoization function implementation
    Implement memoization function to cache results.

---
+++ JS_fundamentals/memoization.js
@@ -0,0 +1,17 @@
+function memoize(fn) {
+    // write code here
+    const cache = {};
+    return function (...args) {
+        const key = JSON.stringify(args);
+        if (key in cache) {
+            return cache[key];
+        } else {
+            const result = fn.apply(this, args)
+            cache[key] = result;
+            return result;
+        }
+    }
+}
+
+memoize();
+module.exports = memoize;
