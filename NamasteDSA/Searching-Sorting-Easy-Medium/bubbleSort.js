9919c17393944c9ced4811180e687fc057e67cd4
Mayuresh Bhagat <mayureshbhagat0@gmail.com>
2026-05-14 16:12:48 +0530

    Add bubble sort implementation in bubbleSort.js
    Implement bubble sort algorithm to sort an array.

---
+++ NamasteDSA/Searching-Sorting-Easy-Medium/bubbleSort.js
@@ -0,0 +1,11 @@
+const arr = [5, 1, 4, 2];
+
+for (let i=0; i<arr.length-1; i++) {
+    for (let j=0; j<arr.length-1-i; j++) {
+        if (arr[j] > arr[j+1]) {
+            [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+        }
+    }
+}
+
+console.log(arr); // [1,2,4,5] tc: O(n) , sc: O(1)
